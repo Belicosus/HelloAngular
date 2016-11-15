2bf6ccf3e7b54336d86d3d34e91fde00a180b7b1
Casper Hougaard Jensen <chj@iterator-it.com>
2016-11-15 11:11:09 +0100

    Tried fixing ignore

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,41 +1,43 @@
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
 
-import {Hero} from './models/hero';
+
+import { Hero } from './models/hero';
+import { HeroService } from './services/hero.service';
 
 @Component({
     moduleId: module.id,
     selector: 'my-app',
     templateUrl: 'app.component.html',
-    styleUrls: ['app.component.css']
+    styleUrls: ['app.component.css'],
+    providers: [HeroService]
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit {
     public title: string = "Tour of Heroes";
 
-    private heroes : Array<Hero>;
-    private selectedHero : Hero;
+    private heroes: Array<Hero>;
+    private selectedHero: Hero;
 
 
-    constructor() {
-        this.heroes = HEROES;
-        //this.selectedHero = new Hero();
+    constructor(
+        private heroService: HeroService
+    ) {
         this.selectedHero = null;
     }
 
-    private onSelect(hero : Hero) {
+    private onSelect(hero: Hero) {
         this.selectedHero = hero;
+    }
+
+    public ngOnInit(): void {
+        this.getHeroes();
+    }
+
+    private getHeroes() {
+        this.heroService.getHeroes().then((result) => {
+            this.heroes = result;
+        });
     }
 }
 
-const HEROES: Hero[] = [
-    { id: 11, name: 'Mr. Nice' },
-    { id: 12, name: 'Narco' },
-    { id: 13, name: 'Bombasto' },
-    { id: 14, name: 'Celeritas' },
-    { id: 15, name: 'Magneta' },
-    { id: 16, name: 'RubberMan' },
-    { id: 17, name: 'Dynama' },
-    { id: 18, name: 'Dr IQ' },
-    { id: 19, name: 'Magma' },
-    { id: 20, name: 'Tornado' }
-];
